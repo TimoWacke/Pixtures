b1d867f97210afed575cf4913171781c6cb771e9
Ace-Of-Snakes <franjolic2307@gmail.com>
2022-11-01 21:40:00 +0100

    cahnges

--- map-face_experimental.py
+++ map-face_experimental.py
@@ -10,14 +10,45 @@ import re
 import sys
 import time
 import cv2
-"""
-requirement: argv[1] is the google map image
-             argv[2] is the user picture
-             argv[3] is the edge length
 
-important output :  third last is width of the image
-                    second last is height of the image
-                    last print is of image path
 """
+    This function is used to load a certain image either from its
+    path or from the standard image. The image is then converted to
+    a PIL image and returned. The image is also determined by the description we input
+    while calling the function.
+"""
+def load_images(description):
+    if (description == "city"):	
+        try:
+            # read the image
+            city_im = Image.open(sys.argv[1])
+            return city_im
+        except:
+            # if the image is not found use the standard image
+            city_im = Image.open("img/city-map-src.png")
+            return city_im
+    if (description == "user"):
+        try:
+            # read the user portrait
+            user_pt = Image.open("/root/Pixtures/img/" + sys.argv[2])
+            user_pt =  ImageOps.exif_transpose(user_pt)
+            return user_pt
+        except:
+            # if the user portrait is not found use the standard image
+            user_pt = Image.open("img/portrait-src.png")
+            return user_pt
+    if (description == "export"):
+        try:
+            exportfile = sys.argv[1][:-4] + sys.argv[2]
+            return exportfile
+        except:
+            exportfile = "img/edit_merged.png"
+        return exportfile
+
 if __name__ == '__main__':
-    print("start")
+
+    im = load_images("city")
+    pt = load_images("user")
+    exportfile = load_images("export")
+    im.save(exportfile)
+    print(exportfile)
