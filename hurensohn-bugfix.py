a6b878e05dee0073495a6616e1673c84f7e70f8a
Timo Wacke <twacke@adobe.com>
2022-11-05 13:53:52 +0100

    -abc

--- hurensohn-bugfix.py
+++ hurensohn-bugfix.py
@@ -1,7 +1,7 @@
 # 👇️ Old import for versions older than Python3.10
 
 # ⛔️ AttributeError: module 'collections' has no attribute 'MutableMapping'
-import collections.abc
+import collections
 print(collections.MutableMapping)
 
 #👇️ New import for versions Python3.10+
