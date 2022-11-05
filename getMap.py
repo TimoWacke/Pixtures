82eb039faf4810125d1005d7607361d6979a3550
Timo Wacke <twacke@adobe.com>
2022-11-05 13:46:00 +0100

    test

--- getMap.py
+++ getMap.py
@@ -16,6 +16,13 @@ import math
 import os
 #some standard imports
 
+import collections 
+if sys.version_info.major == 3 and sys.version_info.minor >= 10:
+
+    from collections.abc import MutableMapping
+else:
+    from collections import MutableMapping
+
 print(sys.version)
 
 #used for standard options
