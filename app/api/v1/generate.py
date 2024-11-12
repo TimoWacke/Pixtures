77bb34504646e2a3496446d8b16f4f18900dcf00
Timo Wacke <wacketimo@gmail.com>
2024-11-12 01:13:43 +0100

    what is the problem

--- app/api/v1/generate.py
+++ app/api/v1/generate.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter
-from concurrent.futures import ThreadPoolExecutor
-import asyncio
 
 from app.services.generate_service import get_generate_service, GenerateRequest
 from app.core.settings import settings
@@ -8,7 +6,6 @@ from app.core.settings import settings
 import logging
 
 router = APIRouter()
-executor = ThreadPoolExecutor(max_workers=1)  # Pool for Selenium tasks
 logger = logging.getLogger(__name__)
 
 
@@ -23,8 +20,7 @@ async def generate_map(
 
         generator = get_generate_service(request.user_id)
 
-        loop = asyncio.get_event_loop()
-        art_piece = await loop.run_in_executor(executor, generator.make_map_art, request)
+        art_piece = generator.make_map_art(request)
 
         return {
             "preview": f"{settings.SELF_DOMAIN}{settings.API_V1_STR}/artpiece/{str(art_piece.id)}",
